a03ef6f4c92f2ac54875a39bb1e33468db87b19b
ericguizzo <eric.guizzo@hotmail.it>
2021-08-26 17:58:42 +0200

    cog-ified

--- predict.py
+++ predict.py
@@ -5,27 +5,34 @@ import zipfile
 from pathlib import Path
 from midi2audio import FluidSynth
 from model import PopMusicTransformer
+import tensorflow as tf
 
 
-class generateMIDI(cog.Predictor):
+class Predictor(cog.Predictor):
     def setup(self):
-        '''Init midi generator and midi-to-wav model'''
-        self.model = PopMusicTransformer(checkpoint='REMI-tempo-checkpoint',
-                                        is_training=False)
+        """Init midi generator and midi-to-wav model"""
+        self.model = PopMusicTransformer(
+            checkpoint="REMI-tempo-checkpoint", is_training=False
+        )
         self.fs = FluidSynth()
 
-    @cog.input("target_bars", type=int, default=16,
-    help="number of midi bars to generate")
-    @cog.input("temperature", type=float, default=1.2,
-    help="stochastic sampling temperature")
-    @cog.input("topk", type=int, default=5,
-    help="top k alternatives for stochastic sampling")
-
-    def predict(self, target_bars, temperature, topk):
+    @cog.input(
+        "target_bars", type=int, default=16, help="number of midi bars to generate"
+    )
+    @cog.input(
+        "temperature", type=float, default=1.2, help="stochastic sampling temperature"
+    )
+    @cog.input(
+        "topk", type=int, default=5, help="top k alternatives for stochastic sampling"
+    )
+    @cog.input("seed", type=int, default=-1, help="Random seed, -1 for random")
+    def predict(self, target_bars, temperature, topk, seed):
         """Generate midi and wav files"""
+        if seed < 0:
+            seed = int.from_bytes(os.urandom(2), "big")
+        tf.random.set_random_seed(seed)
 
         out_path = Path(tempfile.mkdtemp())
-        zip_path = Path(tempfile.mkdtemp()) / "output.zip"
         out_path_midi = out_path / "output.midi"
         out_path_wav = out_path / "output.wav"
 
@@ -35,14 +42,11 @@ class generateMIDI(cog.Predictor):
             temperature=temperature,
             topk=topk,
             output_path=str(out_path_midi),
-            prompt=None)
+            prompt=None,
+        )
         self.model.close()
 
-        #compute soundfile from midi score
+        # compute soundfile from midi score
         self.fs.midi_to_audio(str(out_path_midi), str(out_path_wav))
 
-        with zipfile.ZipFile(str(zip_path), "w") as zf:
-            zf.write(str(out_path_midi))
-            zf.write(str(out_path_wav))
-
-        return zip_path
+        return out_path_wav
